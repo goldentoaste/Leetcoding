0c0c42ab33ddd29ef0063b90124a562a9f578c04
goldentoaste <rayg2375@gmail.com>
2024-06-18 00:46:22 +0800

    combinations

---
+++ 77_combinations.py
@@ -0,0 +1,28 @@
+from typing import List, Set, Dict, Optional
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        out = []
+
+        cur = []
+        def inner(start):
+            if len(cur) == k:
+                out.append(cur[:])
+                return
+
+            for i in range(start, n + 1):
+                # ignore everything after start, to not repeat
+                cur.append(i)
+                inner(i + 1)
+                cur.pop()
+
+        inner(1)
+
+        return out
+
+
+if __name__ == "__main__":
+    o = Solution()
+    print(o.combine(5, 2))
