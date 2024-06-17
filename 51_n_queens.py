496629a79e73bdf0724169937e55281871687fe6
goldentoaste <rayg2375@gmail.com>
2024-06-17 23:13:47 +0800

    less bad queens

--- 51_n_queens.py
+++ 51_n_queens.py
@@ -15,42 +15,43 @@ class Solution:
         def toOutput(bools: List[List[bool]]):
             return tuple(("".join([("Q" if col else ".") for col in row]) for row in bools))
 
-        def nQueens(start, rows: List[bool], cols: List[bool], diag: List[bool], anti_diag: List[bool]):
+        rows, cols, diag, anti_diag = [False] * n, [False] * n, [False] * (n * 2 - 1), [False] * (n * 2 - 1)
+
+        def nQueens(row):
             """
             rows,cols = n items
             diag = n * 2 - 1 items, id is j-i + 9.
             anti_diag = n*2-1 items, i + j
             """
             nonlocal queens
-            for row in range(start, n):
-                for col in range(n):
-                    # if there is already a queen here, dont put it here.
-                    if rows[row] or cols[col] or diag[col - row + n - 1] or anti_diag[row + col]:
-                        continue
-                    # try putting a queen at (row, col)
-                    mem[row][col] = True
-                    rows[row] = True
-                    cols[col] = True
-                    diag[col - row + n - 1] = True
-                    anti_diag[row + col] = True
 
-                    queens += 1
-                    if queens == n:
-                        # solution is found.
-                        out.append(toOutput(mem))
-                    else:
-                        nQueens(row + 1, rows, cols, diag, anti_diag)
+            if row >= n:
+                # solution is found.
+                return out.append(toOutput(mem))
 
-                    # undo the queen placed, try another option next loop
-                    mem[row][col] = False
-                    rows[row] = False
-                    cols[col] = False
-                    diag[col - row + n - 1] = False
-                    anti_diag[row + col] = False
-                    queens -= 1
+            for col in range(n):
+                # if there is already a queen here, dont put it here.
+                if rows[row] or cols[col] or diag[col - row + n - 1] or anti_diag[row + col]:
+                    continue
+                # try putting a queen at (row, col)
+                mem[row][col] = True
+                rows[row] = True
+                cols[col] = True
+                diag[col - row + n - 1] = True
+                anti_diag[row + col] = True
 
-        nQueens(0,[False] * n, [False] * n, [False] * (n * 2 - 1), [False] * (n * 2 - 1))
+                nQueens(row + 1)
+
+                # undo the queen placed, try another option next loop
+                mem[row][col] = False
+                rows[row] = False
+                cols[col] = False
+                diag[col - row + n - 1] = False
+                anti_diag[row + col] = False
+
+        nQueens(0)
         return out
+
 
 if __name__ == "__main__":
     o = Solution()
